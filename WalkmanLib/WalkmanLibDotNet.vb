1d0a385d59b15b635369fa851d9208b50f1087f2
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2018-03-14 22:40:10 +0200

    added functions from PropertiesDotNet

--- WalkmanLib/WalkmanLibDotNet.vb
+++ WalkmanLib/WalkmanLibDotNet.vb
@@ -1,94 +1,151 @@
 ﻿Imports System
 Imports System.IO
+Imports Microsoft.VisualBasic
+Imports System.Security.Principal
+Imports System.Windows.Forms
 Public Partial Class WalkmanLib
-    ''' <summary>
-    ''' 
-    ''' </summary>
+    ''' <summary></summary>
     ''' <param name="path"></param>
     Shared Sub TakeOwnership(path As String)
-        
+        If File.Exists(path) Then
+            RunAsAdmin("cmd.exe", "/c takeown /f " & path & " && icacls " & path & " /grant administrators:F && pause")
+        ElseIf Directory.Exists(path)
+            RunAsAdmin("cmd.exe", "/c takeown /f " & path & " /r /d y && icacls " & path & " /grant administrators:F /t && pause")
+        End If
     End Sub
     
-    ''' <summary>
-    ''' 
-    ''' </summary>
+    ''' <summary></summary>
     ''' <param name="path"></param>
     ''' <returns></returns>
     Shared Function GetFolderIconPath(path As String) As String
         
     End Function
     
-    ''' <summary>
-    ''' 
-    ''' </summary>
-    ''' <param name="path"></param>
-    ''' <param name="fileAttributes"></param>
-    ''' <returns></returns>
+    ''' <summary>Sets the specified System.IO.FileAttributes of the file on the specified path, with a try..catch block.</summary>
+    ''' <param name="path">The path to the file.</param>
+    ''' <param name="fileAttributes">A bitwise combination of the enumeration values.</param>
+    ''' <returns>True if setting the attribute was successful, False if not.</returns>
     Shared Function SetAttribute(path As String, fileAttributes As FileAttributes) As Boolean
-        
+        Try
+            File.SetAttributes(path, fileAttributes)
+            Return True
+        Catch ex As exception
+            ErrorDialog(ex)
+            Return False
+        End Try
     End Function
     
-    ''' <summary>
-    ''' 
-    ''' </summary>
-    ''' <param name="path"></param>
-    ''' <param name="fileAttributes"></param>
-    ''' <returns></returns>
+    ''' <summary>Adds the specified System.IO.FileAttributes to the file on the specified path, with a try..catch block.</summary>
+    ''' <param name="path">The path to the file.</param>
+    ''' <param name="fileAttributes">The FileAttributes to add.</param>
+    ''' <returns>True if adding the attribute was successful, False if not.</returns>
     Shared Function AddAttribute(path As String, fileAttributes As FileAttributes) As Boolean
-        
+        Try
+            File.SetAttributes(path, File.GetAttributes(path) + fileAttributes)
+            Return True
+        Catch ex As exception
+            ErrorDialog(ex)
+            Return False
+        End Try
     End Function
     
-    ''' <summary>
-    ''' 
-    ''' </summary>
-    ''' <param name="path"></param>
-    ''' <param name="fileAttributes"></param>
-    ''' <returns></returns>
+    ''' <summary>Removes the specified System.IO.FileAttributes from the file on the specified path, with a try..catch block.</summary>
+    ''' <param name="path">The path to the file.</param>
+    ''' <param name="fileAttributes">The FileAttributes to remove.</param>
+    ''' <returns>True if removing the attribute was successful, False if not.</returns>
     Shared Function RemoveAttrubute(path As String, fileAttributes As FileAttributes) As Boolean
-        
+        Try
+            File.SetAttributes(path, File.GetAttributes(path) - fileAttributes)
+            Return True
+        Catch ex As exception
+            ErrorDialog(ex)
+            Return False
+        End Try
     End Function
     
-    ''' <summary>
-    ''' 
-    ''' </summary>
-    ''' <param name="path"></param>
-    ''' <param name="arguments"></param>
-    Shared Sub RunAsAdmin(path As String, Optional arguments As String = Nothing)
-        
+    ''' <summary>Starts a program with a set of command-line arguments as an administrator.</summary>
+    ''' <param name="programPath">Path of the program to run as administrator.</param>
+    ''' <param name="arguments">Optional. Command-line arguments to pass when starting the process.</param>
+    Shared Sub RunAsAdmin(programPath As String, Optional arguments As String = Nothing)
+        If arguments = Nothing Then
+            CreateObject("Shell.Application").ShellExecute(programPath, "", "", "runas")
+        Else
+            arguments = arguments.Trim("""")
+            CreateObject("Shell.Application").ShellExecute(programPath, """" & arguments & """", "", "runas")
+        End If
     End Sub
     
-    ''' <summary>
-    ''' 
-    ''' </summary>
-    ''' <param name="path"></param>
+    ''' <summary>Opens the Open With dialog box for a file path.</summary>
+    ''' <param name="path">The file to open with a program.</param>
     Shared Sub OpenWith(path As String)
-        
+        Shell("rundll32 shell32.dll,OpenAs_RunDLL " & path, AppWinStyle.NormalFocus, True, 500)
     End Sub
     
-    ''' <summary>
-    ''' 
-    ''' </summary>
-    ''' <returns></returns>
+    ''' <summary>Checks whether the current process is elevated (running with administrator permissions)</summary>
+    ''' <returns>True if running with administrator permissions, False if not</returns>
     Shared Function IsAdmin() As Boolean
-        
+        Return New WindowsPrincipal(WindowsIdentity.GetCurrent).IsInRole(WindowsBuiltInRole.Administrator)
     End Function
     
-    ''' <summary>
-    ''' 
-    ''' </summary>
+    ''' <summary></summary>
     ''' <param name="text"></param>
     ''' <param name="successMessage"></param>
     ''' <param name="showErrors"></param>
     ''' <returns></returns>
     Shared Function SafeSetText(text As String, Optional successMessage As String = Nothing, Optional showErrors As Boolean = True) As Boolean
-        
+        Try
+            Clipboard.SetText(text, TextDataFormat.UnicodeText)
+            If successMessage <> Nothing Then
+                If successMessage = "default" Then
+                    MsgBox(text & vbNewLine & "Succesfully copied!", MsgBoxStyle.Information, "Succesfully copied!")
+                Else
+                    MsgBox(successMessage, MsgBoxStyle.Information, "Succesfully copied!")
+                End If
+            End If
+            Return True
+        Catch ex As Exception
+            If showErrors Then
+                MsgBox("Copy failed!" & vbNewLine & "Error: """ & ex.ToString & """", MsgBoxStyle.Critical, "Copy failed!")
+            End If
+            Return False
+        End Try
     End Function
     
-    ''' <summary>
-    ''' 
-    ''' </summary>
+    ''' <summary></summary>
     ''' <param name="ex"></param>
     Shared Sub ErrorDialog(ex As Exception)
-        
+        If MsgBox("There was an error! Error message: " & ex.Message & vbNewLine & "Show full stacktrace? (For sending to developer/making bugreport)", _
+          MsgBoxStyle.YesNo + MsgBoxStyle.Exclamation, "Error!") = MsgBoxresult.Yes Then
+            Dim frmBugReport As New Form()
+            frmBugReport.Width = 600
+            frmBugReport.Height = 525
+            frmBugReport.StartPosition = FormStartPosition.CenterParent
+            frmBugReport.WindowState = FormWindowState.Normal
+            frmBugReport.Show()
+            frmBugReport.ShowIcon = False
+            frmBugReport.ShowInTaskbar = True
+            frmBugReport.Text = "Full error trace"
+            Dim txtBugReport As New TextBox()
+            txtBugReport.Multiline = True
+            txtBugReport.ScrollBars = ScrollBars.Vertical
+            frmBugReport.Controls.Add(txtBugReport)
+            txtBugReport.Dock = DockStyle.Fill
+            txtBugReport.Text = "ToString:" & vbNewLine & ex.ToString & vbNewLine & vbNewLine
+            txtBugReport.Text &= "BaseException:" & vbNewLine & ex.GetBaseException.ToString & vbNewLine & vbNewLine
+            txtBugReport.Text &= "Type: " & ex.GetType.ToString & vbNewLine
+            txtBugReport.Text &= "Message: " & ex.Message.ToString & vbNewLine & vbNewLine
+            txtBugReport.Text &= "StackTrace:" & vbNewLine & ex.StackTrace.ToString & vbNewLine & vbNewLine
+            txtBugReport.Text &= "Source: " & ex.Source.ToString & vbNewLine
+            txtBugReport.Text &= "TargetSite: " & ex.TargetSite.ToString & vbNewLine
+            txtBugReport.Text &= "HashCode: " & ex.GetHashCode.ToString & vbNewLine
+            txtBugReport.Text &= "HResult: " & ex.HResult.ToString & vbNewLine & vbNewLine
+            For i = 0 To 100 'Integer.MaxValue no reason to go that high
+                Try
+                    txtBugReport.Text &= "Data:" & vbNewLine & ex.Data(i).ToString & vbNewLine & vbNewLine
+                Catch
+                    Exit For
+                End Try
+            Next
+        End If
     End Sub
 End Class
--- WalkmanLib/WalkmanLibWin32.vb
+++ WalkmanLib/WalkmanLibWin32.vb
@@ -1,17 +1,48 @@
 ﻿Imports System
+Imports System.IO
+Imports System.Runtime.InteropServices
+Imports Microsoft.VisualBasic
 Public Partial Class WalkmanLib
-    ''' <summary>
-    ''' 
-    ''' </summary>
+    ''' <summary></summary>
     ''' <param name="path"></param>
     ''' <returns></returns>
     Shared Function ShowProperties(path As String) As Boolean
-        
+        Dim info As New ShellExecuteInfo
+        info.cbSize = Marshal.SizeOf(info)
+        info.lpVerb = "porperties"
+        info.lpFile = path
+        info.fMask = 12
+        Return ShellExecuteEx(info)
     End Function
+    ' https://stackoverflow.com/a/1936957/2999220
+    <DllImport("shell32.dll", CharSet := CharSet.Auto)> _
+    Private Shared Function ShellExecuteEx(ByRef lpExecInfo As ShellExecuteInfo) As Boolean
+    End Function
+    <StructLayout(LayoutKind.Sequential, CharSet := CharSet.Auto)> _
+    Private Structure ShellExecuteInfo
+        Public cbSize As Integer
+        Public fMask As UInteger
+        Public hwnd As IntPtr
+        <MarshalAs(UnmanagedType.LPTStr)> _
+        Public lpVerb As String
+        <MarshalAs(UnmanagedType.LPTStr)> _
+        Public lpFile As String
+        <MarshalAs(UnmanagedType.LPTStr)> _
+        Public lpParameters As String
+        <MarshalAs(UnmanagedType.LPTStr)> _
+        Public lpDirectory As String
+        Public nShow As Integer
+        Public hInstApp As IntPtr
+        Public lpIDList As IntPtr
+        <MarshalAs(UnmanagedType.LPTStr)> _
+        Public lpClass As String
+        Public hkeyClass As IntPtr
+        Public dwHotKey As UInteger
+        Public hIcon As IntPtr
+        Public hProcess As IntPtr
+    End Structure
     
-    ''' <summary>
-    ''' 
-    ''' </summary>
+    ''' <summary></summary>
     ''' <param name="path"></param>
     ''' <param name="showWindow"></param>
     ''' <returns></returns>
@@ -19,9 +50,7 @@ Public Partial Class WalkmanLib
         
     End Function
     
-    ''' <summary>
-    ''' 
-    ''' </summary>
+    ''' <summary></summary>
     ''' <param name="path"></param>
     ''' <param name="showWindow"></param>
     ''' <returns></returns>
@@ -29,21 +58,47 @@ Public Partial Class WalkmanLib
         
     End Function
     
-    ''' <summary>
-    ''' 
-    ''' </summary>
+        ' usage:
+        'Try
+        '    compressedSizeOrError = CompressedFileSize(lblFullPath.Text)
+        'Catch ex As Exception
+        '    compressedSizeOrError = ex.Message
+        'End Try
+        'chkCompressed.Text = "Compressed"
+    
+    ''' <summary></summary>
     ''' <param name="path"></param>
     ''' <returns></returns>
     Shared Function GetCompressedSize(path As String) As Double
-        
+        Dim sizeMultiplier As IntPtr
+        Dim fileLength As Long = Convert.ToInt64(GetCompressedFileSize(path, sizeMultiplier))
+        If fileLength = 4294967295 Then ' decimal representation of &HFFFFFFFF
+            Dim Err As Long = Marshal.GetLastWin32Error()
+            If Err <> 0 Then Throw New IOException("Exception getting compressed size: " & Err.ToString)
+        End If
+        Dim size As Double = (UInteger.MaxValue + 1) * CLng(sizeMultiplier) + fileLength
+        Return size
     End Function
+    ' http://www.pinvoke.net/default.aspx/kernel32/GetCompressedFileSize.html
+    ' https://stackoverflow.com/a/22508299/2999220
+    Private Declare Function GetCompressedFileSize Lib "kernel32" Alias "GetCompressedFileSizeA"(ByVal lpFileName As String, ByRef lpFileSizeHigh As IntPtr) As UInteger
     
-    ''' <summary>
-    ''' 
-    ''' </summary>
+    ''' <summary></summary>
     ''' <param name="path"></param>
     ''' <returns></returns>
     Shared Function GetOpenWith(path As String) As String
+        Dim pathDirectory As String = New IO.FileInfo(path).DirectoryName
         
+        Dim result As String = Space$(1024)
+        FindExecutable(path, pathDirectory & "\", result)
+        
+        Dim returnString As String = Strings.Left$(result, InStr(result, Chr(0)) - 1)
+        If returnString = "" Then 
+            Return "Filetype not associated!"
+        Else
+            Return returnString
+        End If
     End Function
+    ' http://www.vb-helper.com/howto_get_associated_program.html
+    Private Declare Function FindExecutable Lib "shell32.dll" Alias "FindExecutableA"(lpFile As String, lpDirectory As String, lpResult As String) As Long
 End Class
