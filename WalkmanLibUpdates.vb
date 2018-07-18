d2abf4616f735485940f43acccd7c59f8c99b51a
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2018-07-19 00:11:43 +0200

    added in-line documentation to updates functions

--- WalkmanLibUpdates.vb
+++ WalkmanLibUpdates.vb
@@ -9,11 +9,20 @@ Imports System.Web.Script.Serialization
 
 Public Partial Class WalkmanLib
     Structure VersionInfo
+        ''' <summary>Name of the tag.</summary>
         Public TagName As String
+        
+        ''' <summary>Title of the release.</summary>
         Public Title As String
+        
+        ''' <summary>Body of the release. Consists of raw release text, optionally parse it as markdown before displaying it.</summary>
         Public Body As String
     End Structure
     
+    ''' <summary>Gets information about the latest release in a GitHub project.</summary>
+    ''' <param name="projectName">Name of the project repository.</param>
+    ''' <param name="projectOwner">Owner of the project repository. Default: Walkman100</param>
+    ''' <returns>A WalkmanLib.VersionInfo object populated with information about the latest release.</returns>
     Shared Function GetLatestVersionInfo(projectName As String, Optional projectOwner As String = "Walkman100") As VersionInfo
         ' https://stackoverflow.com/a/2904963/2999220
         ServicePointManager.Expect100Continue = True
@@ -40,6 +49,10 @@ Public Partial Class WalkmanLib
         Return returnVersionInfo
     End Function
     
+    ''' <summary>Gets a download link for the latest installer released in a GitHub project.</summary>
+    ''' <param name="projectName">Name of the project repository.</param>
+    ''' <param name="projectOwner">Owner of the project repository. Default: Walkman100</param>
+    ''' <returns>Download URI in a String.</returns>
     Shared Function GetLatestDownloadLink(projectName As String, Optional projectOwner As String = "Walkman100") As String
         Dim versionString As String
         versionString = GetLatestVersionInfo(projectName, projectOwner).TagName
@@ -47,6 +60,10 @@ Public Partial Class WalkmanLib
         Return "https://github.com/" & projectOwner & "/" & projectName & "/releases/download/" & versionString & "/" & projectName & "-Installer.exe"
     End Function
     
+    ''' <summary>Gets the latest version released in a GitHub project. Note if the tag name is not in version format will throw an Exception.</summary>
+    ''' <param name="projectName">Name of the project repository.</param>
+    ''' <param name="projectOwner">Owner of the project repository. Default: Walkman100</param>
+    ''' <returns>The latest release version parsed as a System.Version object.</returns>
     Shared Function GetLatestVersion(projectName As String, Optional projectOwner As String = "Walkman100") As Version
         Dim versionString As String
         versionString = GetLatestVersionInfo(projectName, projectOwner).TagName
@@ -58,6 +75,11 @@ Public Partial Class WalkmanLib
         Return Version.Parse(versionString)
     End Function
     
+    ''' <summary>Checks if an update release is available in a GitHub project.</summary>
+    ''' <param name="projectName">Name of the project repository.</param>
+    ''' <param name="currentVersion">System.Version to check against.</param>
+    ''' <param name="projectOwner">Owner of the project repository. Default: Walkman100</param>
+    ''' <returns>True if latest version is newer than currentVersion, else False.</returns>
     Shared Function CheckIfUpdateAvailable(projectName As String, currentVersion As Version, Optional projectOwner As String = "Walkman100") As Boolean
         Dim latestVersion As Version
         latestVersion = GetLatestVersion(projectName, projectOwner)
