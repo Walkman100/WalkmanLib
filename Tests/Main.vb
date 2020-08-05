bed5981212b7fbbd01ab28152c314a5dc2f08bc6
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2020-08-05 19:01:21 +0200

    added flag handling using ArgHandler for tests

--- Tests/Main.vb
+++ Tests/Main.vb
@@ -4,17 +4,106 @@ Option Compare Binary
 Option Infer Off
 
 Imports System
+Imports System.Collections.Generic
 
 Module Program
-    Sub Main()
-        If Environment.GetCommandLineArgs().Length = 2 AndAlso Environment.GetCommandLineArgs()(1) = "getAdmin" Then
+    Private flagDict As New Dictionary(Of String, WalkmanLib.FlagInfo) From {
+        {"help", New WalkmanLib.FlagInfo With {
+            .shortFlag = "h"c,
+            .description = "Show Help",
+            .action = Sub()
+                          Console.WriteLine("WalkmanLib Tests. Long options are case-insensitive. All options are True by default, except GUI access." & Environment.NewLine)
+                          WalkmanLib.EchoHelp(flagDict)
+                          Environment.Exit(0)
+                      End Sub
+        }},
+        {"GUI", New WalkmanLib.FlagInfo With {
+            .shortFlag = "g"c,
+            .description = "Set to true if GUI access is available",
+            .hasArgs = True,
+            .argsInfo = "<true|false>",
+            .action = Sub(input As String) haveGUIAccess = getBool(input)
+        }},
+        {"DotNet", New WalkmanLib.FlagInfo With {
+            .shortFlag = "d"c,
+            .description = "Set to false to skip running DotNet tests",
+            .hasArgs = True,
+            .argsInfo = "<true|false>",
+            .action = Sub(input As String) runDotNetTests = getBool(input)
+        }},
+        {"Win32", New WalkmanLib.FlagInfo With {
+            .shortFlag = "w"c,
+            .description = "Set to false to skip running Win32 tests",
+            .hasArgs = True,
+            .argsInfo = "<true|false>",
+            .action = Sub(input As String) runWin32Tests = getBool(input)
+        }},
+        {"Updates", New WalkmanLib.FlagInfo With {
+            .shortFlag = "u"c,
+            .description = "Set to false to skip running Updates tests",
+            .hasArgs = True,
+            .argsInfo = "<true|false>",
+            .action = Sub(input As String) runUpdatesTests = getBool(input)
+        }},
+        {"ArgHandler", New WalkmanLib.FlagInfo With {
+            .shortFlag = "a"c,
+            .description = "Set to false to skip running ArgHandler tests",
+            .hasArgs = True,
+            .argsInfo = "<true|false>",
+            .action = Sub(input As String) runArgHandlerTests = getBool(input)
+        }},
+        {"CustomMsgBox", New WalkmanLib.FlagInfo With {
+            .shortFlag = "c"c,
+            .description = "Set to false to skip running CustomMsgBox tests",
+            .hasArgs = True,
+            .argsInfo = "<true|false>",
+            .action = Sub(input As String) runCustomMsgBoxTests = getBool(input)
+        }}
+    }
+
+    Private haveGUIAccess As Boolean = False
+    Private runDotNetTests As Boolean = True
+    Private runWin32Tests As Boolean = True
+    Private runUpdatesTests As Boolean = True
+    Private runArgHandlerTests As Boolean = True
+    Private runCustomMsgBoxTests As Boolean = True
+
+    Private Function getBool(input As String) As Boolean
+        Dim rtn As Boolean
+        If Boolean.TryParse(input, rtn) Then
+            Return rtn
+        Else
+            ExitE("""{0}"" is not True or False!", input)
+            Environment.Exit(1)
+            End
+        End If
+    End Function
+
+    Sub Main(args As String())
+        Dim res As WalkmanLib.ResultInfo = WalkmanLib.ProcessArgs(args, flagDict, True)
+
+        If res.gotError Then
+            ExitE(res.errorInfo)
+        ElseIf res.extraParams.Count = 1 AndAlso res.extraParams.Item(0) = "getAdmin" Then
             Console.WriteLine(WalkmanLib.IsAdmin)
             Environment.Exit(0)
+        ElseIf res.extraParams.Count > 0 Then
+            ExitE("Unknown parameter supplied!")
         End If
-        
-        Console.WriteLine(Microsoft.VisualBasic.vbNewLine & "All tests completed successfully: " & Tests.RunTests())
-        
+
+        Console.WriteLine(Environment.NewLine & "All tests completed successfully: " & Tests.RunTests(haveGUIAccess,
+            runCustomMsgBoxTests, runArgHandlerTests, runUpdatesTests, runWin32Tests, runDotNetTests))
+
         Console.Write("Press any key to continue . . . ")
         Console.ReadKey(True)
+        Console.WriteLine()
+    End Sub
+
+    Sub ExitE(msg As String, Optional formatItem As String = Nothing)
+        If formatItem IsNot Nothing Then
+            msg = String.Format(msg, formatItem)
+        End If
+        Console.WriteLine(msg)
+        Environment.Exit(1)
     End Sub
 End Module
