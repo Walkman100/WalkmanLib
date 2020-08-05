20d5e5171c18471f1176fd59cf64454caa68654a
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2020-08-05 15:37:38 +0200

    Added 32 and 64-bit target platforms, set AnyCPU to prefer 64-bit, added Full debug on Release target

--- Tests/Main.vb
+++ Tests/Main.vb
@@ -7,6 +7,11 @@ Imports System
 
 Module Program
     Sub Main()
+        If Environment.GetCommandLineArgs().Length = 2 AndAlso Environment.GetCommandLineArgs()(1) = "getAdmin" Then
+            Console.WriteLine(WalkmanLib.IsAdmin)
+            Environment.Exit(0)
+        End If
+        
         Console.WriteLine(Microsoft.VisualBasic.vbNewLine & "All tests completed successfully: " & Tests.RunTests())
         
         Console.Write("Press any key to continue . . . ")
