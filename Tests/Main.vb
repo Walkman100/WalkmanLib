f32f9cae40a4521db3f2d05af5501db1baa0ffc6
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2020-08-05 15:09:49 +0200

    added files to project, added call to tests

--- Tests/Main.vb
+++ Tests/Main.vb
@@ -1,8 +1,13 @@
+Option Explicit On
+Option Strict On
+Option Compare Binary
+Option Infer Off
+
+Imports System
+
 Module Program
     Sub Main()
-        Console.WriteLine("Hello World!")
-        
-        ' TODO: Implement Functionality Here
+        Console.WriteLine(Microsoft.VisualBasic.vbNewLine & "All tests completed successfully: " & Tests.RunTests())
         
         Console.Write("Press any key to continue . . . ")
         Console.ReadKey(True)
