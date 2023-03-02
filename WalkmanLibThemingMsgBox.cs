f45437aa7e6b78f5072e2da025a8cf79eff10a5b
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2023-03-02 18:00:53 +0200

    added themed version of CustomMsgBoxBTN

--- WalkmanLibThemingMsgBox.cs
+++ WalkmanLibThemingMsgBox.cs
@@ -55,6 +55,25 @@ public partial class WalkmanLib {
         return formToShow.DialogResultString;
     }
 
+    public static byte CustomMsgBoxBTN(string text, Theme theme, string caption, string customButton1, string customButton2 = null, string customButton3 = null,
+                                       MessageBoxIcon style = 0, WinVersionStyle winVersion = WinVersionStyle.Win10, Form ownerForm = null) {
+        var formToShow = new CustomMsgBoxForm() {
+            Prompt = text,
+            Title = caption,
+            Button1Text = customButton1,
+            Button2Text = customButton2,
+            Button3Text = customButton3,
+            FormLevel = style,
+            WinVersion = winVersion,
+            Owner = ownerForm,
+            ShowInTaskbar = false
+        };
+        formToShow.ApplyTheme(theme);
+        formToShow.ShowDialog();
+
+        return formToShow.DialogResultButtonPressed;
+    }
+
     public static DialogResult InputDialog(ref string input, Theme theme, string mainInstruction = null, string title = null, string content = null,
                                            bool usePasswordMasking = false, int maxLength = short.MaxValue, Form owner = null) {
         var inputForm = new CustomInputDialog() {
