713261a6226a3d31ab57da6dfb9a6d7643b529fc
░▒▓█│【Walkman】│█▓▒░ <matthewcarter17@gmail.com>
2023-03-02 18:25:35 +0200

    added themed version of WalkmanLib.ErrorDialog()

--- WalkmanLibThemingMsgBox.cs
+++ WalkmanLibThemingMsgBox.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Windows.Forms;
 
 public partial class CustomMsgBoxForm {
@@ -91,5 +92,68 @@ public partial class WalkmanLib {
             input = inputForm.Input;
 
         return result;
+    }
+
+    public static void ErrorDialog(Exception ex, Theme theme, string errorMessage = "There was an error! Error message: ", bool showMsgBox = true, Form ownerForm = null, bool? showErrorBlockingWindow = null) {
+        Application.EnableVisualStyles(); // affects when in a console app
+        if (showMsgBox && WalkmanLib.CustomMsgBox($"{errorMessage}{ex.Message}{Environment.NewLine}Show full stacktrace? (For sending to developer/making bugreport)",
+                                                  theme, "Error!", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation, ownerForm: ownerForm) != DialogResult.Yes)
+            return;
+
+        var frmBugReport = new Form() {
+            Width = 600,
+            Height = 525,
+            StartPosition = FormStartPosition.CenterParent,
+            ShowIcon = false,
+            Text = "Full error trace"
+        };
+        var txtBugReport = new TextBox() {
+            Multiline = true,
+            ScrollBars = ScrollBars.Vertical,
+            Dock = DockStyle.Fill
+        };
+        frmBugReport.Controls.Add(txtBugReport);
+        WalkmanLib.ApplyTheme(theme, frmBugReport);
+
+        try {
+            txtBugReport.Text = "";
+            while (ex != null) {
+                if (ex.ToString() != null)          txtBugReport.Text += $"ToString:{Environment.NewLine}{ex}{Environment.NewLine}{Environment.NewLine}";
+                if (ex.GetBaseException() != null)  txtBugReport.Text += $"BaseException:{Environment.NewLine}{ex.GetBaseException()}{Environment.NewLine}{Environment.NewLine}";
+                if (ex.GetType() != null)           txtBugReport.Text += $"Type: {ex.GetType()}{Environment.NewLine}";
+                if (ex.Message != null)             txtBugReport.Text += $"Message: {ex.Message}{Environment.NewLine}{Environment.NewLine}";
+                if (ex.StackTrace != null)          txtBugReport.Text += $"StackTrace:{Environment.NewLine}{ex.StackTrace}{Environment.NewLine}{Environment.NewLine}";
+                if (ex is System.ComponentModel.Win32Exception win32ex) {
+                                                    txtBugReport.Text += $"ErrorCode: 0x{win32ex.ErrorCode.ToString("X")}{Environment.NewLine}";
+                                                    txtBugReport.Text += $"NativeErrorCode: 0x{win32ex.NativeErrorCode.ToString("X")}{Environment.NewLine}";
+                }
+                if (ex is System.IO.FileNotFoundException fileNotFoundEx) {
+                                                    txtBugReport.Text += $"FileName: {fileNotFoundEx.FileName}{Environment.NewLine}";
+                                                    txtBugReport.Text += $"FusionLog: {fileNotFoundEx.FusionLog}{Environment.NewLine}";
+                }
+                if (ex.Source != null)              txtBugReport.Text += $"Source: {ex.Source}{Environment.NewLine}";
+                if (ex.TargetSite != null)          txtBugReport.Text += $"TargetSite: {ex.TargetSite}{Environment.NewLine}";
+                                                    txtBugReport.Text += $"HashCode: 0x{ex.GetHashCode().ToString("X")}{Environment.NewLine}";
+                                                    txtBugReport.Text += $"HResult: 0x{ex.HResult.ToString("X")}{Environment.NewLine}{Environment.NewLine}";
+                foreach (object key in ex.Data.Keys) {
+                                                    txtBugReport.Text += $"Data({key}): {ex.Data[key]}{Environment.NewLine}";
+                }
+                if (ex.InnerException != null)      txtBugReport.Text += $"{Environment.NewLine}InnerException:{Environment.NewLine}";
+                ex = ex.InnerException;
+            }
+        } catch (Exception ex2) {
+            txtBugReport.Text += $"Error getting exception data!{Environment.NewLine}{Environment.NewLine}{ex2}";
+        }
+
+        try {
+            if (showErrorBlockingWindow.HasValue && showErrorBlockingWindow.Value)
+                frmBugReport.ShowDialog();
+            else if (showErrorBlockingWindow.HasValue)
+                frmBugReport.Show();
+            else
+                System.Threading.Tasks.Task.Run(() => frmBugReport.ShowDialog());
+        } catch (Exception ex2) {
+            MessageBox.Show($"Error showing window: {ex2}", Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
     }
 }
